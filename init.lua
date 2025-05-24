7e2a478eb9a91e5f435f5eee79720e1b9037912a
Guilherme Menezes <guilhermedeoliveira.menezes@gmail.com>
2025-05-24 13:16:57 -0300

    update

--- init.lua
+++ init.lua
@@ -6,6 +6,77 @@ require('nixCatsUtils').setup {
   non_nix_value = true,
 }
 
+local servers = {}
+if require('nixCatsUtils').isNixCats then
+  -- nixd requires some configuration.
+  -- luckily, the nixCats plugin is here to pass whatever we need!
+  -- we passed this in via the `extra` table in our packageDefinitions
+  -- for additional configuration options, refer to:
+  -- https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md
+  servers.nixd = {
+    settings = {
+      nixd = {
+        nixpkgs = {
+          -- in the extras set of your package definition:
+          -- nixdExtras.nixpkgs = ''import ${pkgs.path} {}''
+          expr = nixCats.extra 'nixdExtras.nixpkgs' or [[import <nixpkgs> {}]],
+        },
+        options = {
+          -- If you integrated with your system flake,
+          -- you should use inputs.self as the path to your system flake
+          -- that way it will ALWAYS work, regardless
+          -- of where your config actually was.
+          nixos = {
+            -- nixdExtras.nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options''
+            expr = nixCats.extra 'nixdExtras.nixos_options',
+          },
+          -- If you have your config as a separate flake, inputs.self would be referring to the wrong flake.
+          -- You can override the correct one into your package definition on import in your main configuration,
+          -- or just put an absolute path to where it usually is and accept the impurity.
+          ['home-manager'] = {
+            -- nixdExtras.home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options''
+            expr = nixCats.extra 'nixdExtras.home_manager_options',
+          },
+        },
+        formatting = {
+          command = { 'nixfmt' },
+        },
+        diagnostic = {
+          suppress = {
+            'sema-escaping-with',
+          },
+        },
+      },
+    },
+  }
+else
+  servers.rnix = {}
+  servers.nil_ls = {}
+end
+
+-- Check if we used nix, only run mason if we did not
+if require('nixCatsUtils').isNixCats then
+  for server_name, cfg in pairs(servers) do
+    -- This gets provided a default configuration by nvim-lspconfig
+    -- and then ours gets tbl_deep_extend'ed into it
+    vim.lsp.config(server_name, cfg)
+    vim.lsp.enable(server_name)
+  end
+else
+  require('mason').setup()
+  local mason_lspconfig = require 'mason-lspconfig'
+  mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers),
+  }
+  mason_lspconfig.setup_handlers {
+    function(server_name)
+      local server = servers[server_name] or {}
+      vim.lsp.config(server_name, server)
+      vim.lsp.enable(server_name)
+    end,
+  }
+end
+
 -- NOTE: You might want to move the lazy-lock.json file
 local function getlockfilepath()
   if require('nixCatsUtils').isNixCats and type(nixCats.settings.unwrappedCfgPath) == 'string' then
