0e1920fbb1fc38cf59215f6b6ce8e4043f693960
Guilherme Menezes <guilhermedeoliveira.menezes@gmail.com>
2025-07-08 22:45:17 -0300

    fix: nixd

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -13,43 +13,46 @@ return {
     { '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', mode = 'i' },
   },
   opts = {
-    nixd = {
-      settings = {
-        nixd = {
-          nixpkgs = {
-            -- in the extras set of your package definition:
-            -- nixdExtras.nixpkgs = ''import ${pkgs.path} {}''
-            expr = nixCats.extra 'nixdExtras.nixpkgs' or [[import <nixpkgs> {}]],
-          },
-          options = {
-            -- If you integrated with your system flake,
-            -- you should use inputs.self as the path to your system flake
-            -- that way it will ALWAYS work, regardless
-            -- of where your config actually was.
-            nixos = {
-              -- nixdExtras.nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options''
-              expr = nixCats.extra 'nixdExtras.nixos_options',
+    servers = {
+      nixd = {
+        settings = {
+          nixd = {
+            nixpkgs = {
+              -- in the extras set of your package definition:
+              -- nixdExtras.nixpkgs = ''import ${pkgs.path} {}''
+              expr = nixCats.extra 'nixdExtras.nixpkgs' or [[import <nixpkgs> {}]],
             },
-            -- If you have your config as a separate flake, inputs.self would be referring to the wrong flake.
-            -- You can override the correct one into your package definition on import in your main configuration,
-            -- or just put an absolute path to where it usually is and accept the impurity.
-            ['home-manager'] = {
-              -- nixdExtras.home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options''
-              expr = nixCats.extra 'nixdExtras.home_manager_options',
+            options = {
+              -- If you integrated with your system flake,
+              -- you should use inputs.self as the path to your system flake
+              -- that way it will ALWAYS work, regardless
+              -- of where your config actually was.
+              nixos = {
+                -- nixdExtras.nixos_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").nixosConfigurations.configname.options''
+                expr = nixCats.extra 'nixdExtras.nixos_options',
+              },
+              -- If you have your config as a separate flake, inputs.self would be referring to the wrong flake.
+              -- You can override the correct one into your package definition on import in your main configuration,
+              -- or just put an absolute path to where it usually is and accept the impurity.
+              ['home-manager'] = {
+                -- nixdExtras.home_manager_options = ''(builtins.getFlake "path:${builtins.toString inputs.self.outPath}").homeConfigurations.configname.options''
+                expr = nixCats.extra 'nixdExtras.home_manager_options',
+              },
             },
-          },
-          formatting = {
-            command = { 'nixfmt' },
-          },
-          diagnostic = {
-            suppress = {
-              'sema-escaping-with',
+            formatting = {
+              command = { 'nixfmt' },
+            },
+            diagnostic = {
+              suppress = {
+                'sema-escaping-with',
+              },
             },
           },
         },
       },
-    },
-    servers = {
+      nil_ls = {
+        enabled = false,
+      },
       elixirls = {
         cmd = { 'elixir-ls' },
         keys = {
