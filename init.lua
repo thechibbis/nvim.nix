f6f45e8527d49c4f8b0bf88e3a8870e50c437ca8
Guilherme Menezes <guilhermedeoliveira.menezes@gmail.com>
2025-07-16 20:18:39 -0300

    prisma

--- init.lua
+++ init.lua
@@ -65,6 +65,8 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
   { import = 'lazyvim.plugins.extras.lsp.none-ls' },
 
   { import = 'lazyvim.plugins.extras.lang.typescript' },
+  { import = 'lazyvim.plugins.extras.lang.tailwind' },
+  { import = 'lazyvim.plugins.extras.lang.prisma' },
   { import = 'lazyvim.plugins.extras.lang.rust' },
   { import = 'lazyvim.plugins.extras.lang.go' },
   { import = 'lazyvim.plugins.extras.lang.docker' },
